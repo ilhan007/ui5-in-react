2408248e560be19fa70dd4b150bc320c979440b9
ilhan <ilhan.myumyun@sap.com>
2019-02-17 19:32:55 +0200

    First pages commit

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import 'babel-polyfill';
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app/App.js";
-
-ReactDOM.render(<App />, document.getElementById("root"));
